0147ceb066d9f06909cbf8210334a7fc0bbb189b
SiddiquiArfat <arfatsiddiqui070@gmail.com>
2023-10-01 23:11:35 +0530

    "Basin functionality"

--- backend/Task/src/main/java/com/Task/Task/Service/UserService.java
+++ backend/Task/src/main/java/com/Task/Task/Service/UserService.java
@@ -1,10 +1,11 @@
 package com.Task.Task.Service;
 
+import com.Task.Task.Model.AssignTaskDTO;
 import com.Task.Task.Model.Project;
 import com.Task.Task.Model.Task;
 import com.Task.Task.Model.TaskUser;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
 
 public interface UserService {
@@ -12,5 +13,22 @@ public interface UserService {
     public Project addProject(Integer uid, Project project);
     public Task addTask(Integer uid, Integer pid, Task task);
 
-    public Task assignTask(Integer uid, Integer pid, Integer tid);
+//    public Task assignTask(Integer uid, Integer pid, Integer tid);
+
+    public AssignTaskDTO assignTask(AssignTaskDTO dto);
+    public List<Task> getAllTasks(Integer uid);
+
+    public List<Task> openedTask(Integer uid);
+    public List<Task> closedTask(Integer uid);
+
+    public Task closeTask(AssignTaskDTO dto);
+
+    Task openTask(AssignTaskDTO dto);
+
+    public List<Project> getAllProjects(Integer uid);
+
+    public TaskUser searchByUsername(String username);
+
+
+
 }
--- backend/Task/src/main/java/com/Task/Task/Service/UserServiceImpl.java
+++ backend/Task/src/main/java/com/Task/Task/Service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.Task.Task.Service;
 
+import com.Task.Task.Model.AssignTaskDTO;
 import com.Task.Task.Model.Project;
 import com.Task.Task.Model.Task;
 import com.Task.Task.Model.TaskUser;
@@ -10,12 +11,11 @@ import com.Task.Task.Respository.UserRepository;
 import com.Task.Task.exception.ProjectException;
 import com.Task.Task.exception.TaskException;
 import com.Task.Task.exception.UserException;
-import jdk.jshell.spi.ExecutionControl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 
@@ -60,25 +60,83 @@ public class UserServiceImpl  implements UserService{
     }
 
 
-//    need updated
-
     @Override
-    public Task assignTask(Integer uid, Integer pid, Integer tid) {
-        TaskUser user = ur.findById(uid).orElseThrow(()-> new UserException("User Not Found"));
-        Project project = pr.findById(pid).orElseThrow(()->new ProjectException("Project Not Found"));
-        Task task = tr.findById(tid).orElseThrow(()-> new TaskException("Task Not Found"));
-        List<TaskUser> users = project.getUser();
-        boolean bool = users.stream().anyMatch(u -> Objects.equals(u.getId(), user.getId()));
-        if(bool){
-            task.setUser(user);
-            List<Task> tasks = user.getAssignedTasks();
-            tasks.add(task);
-            user.setAssignedTasks(tasks);
-            ur.save(user);
-            return tr.save(task);
-        }
-        throw new ProjectException("Project Not Found");
+    public AssignTaskDTO assignTask(AssignTaskDTO dto) {
+        TaskUser user = ur.findByUsername(dto.getUsername()).orElseThrow(()->new UserException("User not Found"));
+        Task task = tr.findById(dto.getTaskId()).orElseThrow(()->new TaskException("Task Not Found"));
+        List<Task> tasks = user.getAssignedTasks();
+        tasks.add(task);
+        user.setAssignedTasks(tasks);
+        task.setUser(user);
+        tr.save(task);
+        return dto;
     }
 
+    @Override
+    public List<Task> getAllTasks(Integer uid) {
+        TaskUser user = ur.findById(uid).orElseThrow(()->new UserException("User not Found"));
+        List<Task> tasks = user.getAssignedTasks();
+        if(tasks.isEmpty()) throw new TaskException("No Current Tasks");
+        return tasks;
+    }
+
+    @Override
+    public List<Task> openedTask(Integer uid) {
+        TaskUser user = ur.findById(uid).orElseThrow(()->new UserException("User not Found"));
+        List<Task> tasks = user.getAssignedTasks();
+        if(tasks.isEmpty()) throw new TaskException("No Current Tasks");
+        List<Task> tasks1 = tasks.stream().filter(h -> h.getStatus().equals("OPEN")).toList();
+        if(tasks1.isEmpty()) throw new TaskException("No Open Current Tasks");
+        return tasks1;
+    }
+
+    @Override
+    public List<Task> closedTask(Integer uid) {
+        TaskUser user = ur.findById(uid).orElseThrow(()->new UserException("User not Found"));
+        List<Task> tasks = user.getAssignedTasks();
+        if(tasks.isEmpty()) throw new TaskException("No Current Tasks");
+        List<Task> tasks1 = tasks.stream().filter(h -> h.getStatus().equals("CLOSED")).toList();
+        if(tasks1.isEmpty()) throw new TaskException("No Open Current Tasks");
+        return tasks1;
+    }
+
+    @Override
+    public Task closeTask(AssignTaskDTO dto) {
+//        TaskUser user = ur.findByUsername(dto.getUsername()).orElseThrow(()->new UserException("User not Found"));
+        Task task = tr.findById(dto.getTaskId()).orElseThrow(()->new TaskException("Task Not Found"));
+        task.setStatus("CLOSED");
+        task.setClosed(LocalDate.now());
+        return tr.save(task);
+    }
+
+    @Override
+    public Task openTask(AssignTaskDTO dto) {
+//        TaskUser user = ur.findByUsername(dto.getUsername()).orElseThrow(()->new UserException("User not Found"));
+        Task task = tr.findById(dto.getTaskId()).orElseThrow(()->new TaskException("Task Not Found"));
+        task.setStatus("OPEN");
+        task.setClosed(null);
+        return tr.save(task);
+    }
+
+
+    @Override
+    public List<Project> getAllProjects(Integer uid) {
+        TaskUser user = ur.findById(uid).orElseThrow(()->new UserException("User not Found"));
+        List<Project> projects = user.getProject();
+        if(projects.isEmpty()) throw new ProjectException("No Projects Found");
+        return projects;
+    }
+
+    @Override
+    public TaskUser searchByUsername(String username) {
+        return ur.findByUsername(username).orElseThrow(()-> new UserException("User Not Found"));
+    }
+
+    public TaskUser follow(Integer uid, AssignTaskDTO dto){
+
+        TaskUser user = ur.findByUsername(dto.getUsername()).orElseThrow(()-> new UserException("User Not Found"));
+
+
+    }
 
 }
